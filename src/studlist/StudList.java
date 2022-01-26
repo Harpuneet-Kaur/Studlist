611bb934a869e4dba1629b279f52f9fd1fbf6fb4
Harpuneet <kau10474@sheridancollege.ca>
2022-01-26 15:24:08 -0500

    checking changes

--- src/studlist/StudList.java
+++ src/studlist/StudList.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class StudList {
 
     /**
+     * author @harpuneet
      * @param args the command line arguments
      */
     public static void main(String[] args) {
